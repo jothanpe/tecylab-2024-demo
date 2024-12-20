9aa4411531d8a9bf8c1647a19c6adee4f05969c9
JothanPE <jothanpe@gmail.com>
2024-12-19 20:58:18 -0500

    Demo V2

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,12 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth } from "@aws-amplify/backend";
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      verificationEmailStyle: "CODE",
+      verificationEmailSubject: "AI-Powered Recipe Generator App!",
+      verificationEmailBody: (createCode) =>
+        `Usa este código para confirmar tu cuenta: ${createCode()}`,
+    },
   },
 });
